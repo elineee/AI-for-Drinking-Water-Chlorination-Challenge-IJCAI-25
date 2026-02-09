92b79ff8406a1ef72bcf6e4362f4fe676d4ed223
Eline Mota <eline.mota01@gmail.com>
2026-02-09 15:18:16 +0100

    transform data

---
+++ exploration/transformation.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+def change_data_format(file_name):
+    df = pd.read_csv(file_name)
+    
+    df_cleaned = df.drop(columns=["pressure [meter] at 10","pressure [meter] at 11","pressure [meter] at 12","pressure [meter] at 13","pressure [meter] at 21","pressure [meter] at 22","pressure [meter] at 23","pressure [meter] at 31","pressure [meter] at 32","pressure [meter] at 9","pressure [meter] at 2","flow [cubic meter/hr] at 10","flow [cubic meter/hr] at 11","flow [cubic meter/hr] at 12","flow [cubic meter/hr] at 21","flow [cubic meter/hr] at 22","flow [cubic meter/hr] at 31","flow [cubic meter/hr] at 110","flow [cubic meter/hr] at 111","flow [cubic meter/hr] at 112","flow [cubic meter/hr] at 113","flow [cubic meter/hr] at 121","flow [cubic meter/hr] at 122","flow [cubic meter/hr] at 9"])
+    
+    print(df_cleaned.T)
+    # transform the data to have one row = one time step, the chlorine value, the arsenic value and the number of the node 
+    new_data = {
+        "timestep": [],
+        "node": [],
+        "chlorine_concentration": [],
+        "arsenic_concentration": []
+    }
+    
+    nodes = set()
+    
+    for column in df_cleaned.columns:
+        number = get_node_number(column)
+        nodes.add(number)
+        
+    for timestep, row in df_cleaned.iterrows():
+        for node in nodes:
+            chlorine_col = f'bulk_species_node [MG] at Chlorine @ {node}'
+            arsenic_col = f'bulk_species_node [MG] at AsIII @ {node}'
+            
+            if chlorine_col in df_cleaned.columns and arsenic_col in df_cleaned.columns:
+                new_data["timestep"].append(timestep)
+                new_data["node"].append(node)
+                new_data["chlorine_concentration"].append(row[chlorine_col])
+                new_data["arsenic_concentration"].append(row[arsenic_col])
+        
+    new_df = pd.DataFrame(new_data)
+    
+    new_df.to_csv(file_name.replace(".csv", "_cleaned.csv"), index=False)
+    
+
+def get_node_number(column_name):
+    # extract the number from the column name to get the node number
+    return int(column_name.split(" @ ")[1].split(" ")[0])
+
+if __name__ == "__main__":
+    change_data_format(".\data\data_arsenic\scada_data_100_contamination.csv")
+    
