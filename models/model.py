2f34a0265526103f988fb51212a89bb1883f5eea
Eline Mota <eline.mota01@gmail.com>
2026-02-11 16:30:48 +0100

    add spec class model

--- models/model.py
+++ models/model.py
@@ -4,12 +4,14 @@ from abc import ABC, abstractmethod
 
 from experiment_config import ExperimentConfig
 
+""" Abstract class for anomaly detection models. All specific models (e.g., LOF, Isolation Forest) should inherit from this class and implement the `get_results` method, which should return the true labels and predicted labels for the experiment."""
 class AnomalyModel(ABC):
     def __init__(self, config: ExperimentConfig):
         self.config = config
         
     @abstractmethod
     def get_results(self):
+        """ Return y_true and y_pred for the experiment, where y_true are the true labels and y_pred are the predicted labels by the model. """
         pass
 
     
