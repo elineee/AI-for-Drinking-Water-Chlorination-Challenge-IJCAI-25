6941b7ced723bbc092d1303933b28a24d9752ecc
AurelieGnt <genot.aurelie.7@gmail.com>
2026-02-16 10:49:03 +0100

    Add evaluation methods

--- models/evaluation.py
+++ models/evaluation.py
@@ -1,6 +1,22 @@
-from sklearn.metrics import accuracy_score, confusion_matrix, recall_score, f1_score
+from enum import Enum
+from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score, confusion_matrix, recall_score, f1_score
 import pickle
+import matplotlib.pyplot as plt
+import numpy as np
 
+class Metrics(Enum):
+    """ Enumeration of evaluation metrics"""
+    ACCURACY = "accuracy"
+    CONFUSION_MATRIX = "confusion_matrix"
+    RECALL = "recall"
+    F1_SCORE = "f1_score"
+
+
+class ScalarMetrics(Enum):
+    """ Enumeration of scalar evaluation metrics (metrics that can be averaged across nodes)"""
+    ACCURACY = "accuracy"
+    RECALL = "recall"   
+    F1_SCORE = "f1_score"
 
 class Evaluation:
     """
@@ -12,7 +28,7 @@ class Evaluation:
 
     def evaluate(self, results_file: str):
         """
-        Evaluates the results of the experiments by calculating accuracy, confusion matrix, recall and F1 score for each experiment configuration. 
+        Evaluates the results of the experiments by calculating metrics for each experiment configuration. 
         The results are loaded from a pickle file containing the results of the experiments.
 
         Parameters:
@@ -38,10 +54,86 @@ class Evaluation:
                     y_pred = values["y_pred"]
                
                     evaluation_results[config_name][node] = {
-                        "accuracy": accuracy_score(y_true, y_pred),
-                        "confusion_matrix": confusion_matrix(y_true, y_pred, labels=[1, -1]),
-                        "recall": recall_score(y_true, y_pred, pos_label=-1),
-                        "f1_score": f1_score(y_true, y_pred, pos_label=-1)
+                        Metrics.ACCURACY.value : accuracy_score(y_true, y_pred),
+                        Metrics.CONFUSION_MATRIX.value: confusion_matrix(y_true, y_pred, labels=[1, -1]),
+                        Metrics.RECALL.value: recall_score(y_true, y_pred, pos_label=-1),
+                        Metrics.F1_SCORE.value: f1_score(y_true, y_pred, pos_label=-1)
                     }
 
         return evaluation_results
+
+    def plot_confusion_matrices(self, config_name: str, evaluation_results: dict):
+        """
+        Plots the confusion matrices for a specific configuration. If the configuration contains multiple nodes, it plots a confusion matrix for each node.
+        This allows to visualize the performance of the model in terms of true positives, true negatives, false positives and false negatives for each configuration and node.
+
+        Parameters:
+        - config_name: the name of the configuration for which to plot the confusion matrices.
+        - evaluation_results: the dictionary containing the evaluation results for each experiment configuration and node, as returned by the evaluate method.
+        """
+
+        if config_name not in evaluation_results:
+            raise ValueError(f"{config_name} not found in evaluation results.")
+
+        nodes_dict = evaluation_results[config_name]
+
+        for node in nodes_dict:
+            cm = nodes_dict[node][Metrics.CONFUSION_MATRIX.value]
+            display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Normal (1)', 'Anomaly (-1)'])
+            display.plot(cmap=plt.cm.Blues)
+            plt.title(f'Confusion Matrix for {config_name} - Node {node}')
+            plt.show()
+
+
+    def plot_results_nodes_within_configurations(self, metric_name: ScalarMetrics, evaluation_results: dict):
+        """
+        Plots the results for each configuration of the different nodes for the specified metric. 
+        It allows to compare the performance of the model across different nodes for the same configuration.
+
+        Parameters:
+        - metric_name: the name of the metric to plot (ScalarMetrics)
+        - evaluation_results: the dictionary containing the evaluation results for each experiment configuration and node, as returned by the evaluate method.
+        """
+
+        for config_name in evaluation_results:
+            nodes_dict = evaluation_results[config_name]
+
+            metric_values = [nodes_dict[node][metric_name.value] for node in nodes_dict]
+            nodes = list(nodes_dict.keys())
+
+            plt.bar(nodes, metric_values)
+            plt.xlabel('Nodes')
+            plt.ylabel(metric_name.value)
+            plt.title(f'{metric_name.value} for each node in configuration {config_name}')
+            plt.tight_layout()
+            plt.show()
+
+    def plot_mean_configuration(self, metric_name: ScalarMetrics, evaluation_results: dict):  
+        """
+        Plots the performance of each configuration by giving the mean of nodes for the specified metric.
+        This allows to compare the overall performance of different models on the same metric, by aggregating the results across all nodes.
+
+        Parameters:
+        - metric_name: the name of the metric to plot (ScalarMetrics)
+        - evaluation_results: the dictionary containing the evaluation results for each experiment configuration and node, as returned by the evaluate method.
+        """
+
+        config_names = []
+        mean_metric_values = []
+
+        for config_name in evaluation_results:
+            nodes_dict = evaluation_results[config_name]
+
+            metric_values = [nodes_dict[node][metric_name.value] for node in nodes_dict]
+            mean_metric_value = np.mean(metric_values)
+
+            config_names.append(config_name)
+            mean_metric_values.append(mean_metric_value)
+
+        plt.bar(config_names, mean_metric_values)
+        plt.xlabel('Configurations')
+        plt.ylabel(f'Mean {metric_name.value}')
+        plt.title(f'Mean {metric_name.value} for each configuration')
+        plt.tight_layout()
+        plt.show()
+
