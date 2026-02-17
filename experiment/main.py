828ff4180245db66ee35b69c39c70baabf8ca096
Eline Mota <eline.mota01@gmail.com>
2026-02-17 11:39:09 +0100

    add svr part 1

--- experiment/main.py
+++ experiment/main.py
@@ -9,40 +9,59 @@ if __name__ == "__main__":
     CONTAMINATED_FILES = [".\\data\\data_arsenic\\scada_data_conta_22.csv"]
 
     configs = [
+        # ExperimentConfig(
+        #     config_name="LOF_20_neighbors",
+        #     example_files=CLEAN_FILES,
+        #     contaminated_files=CONTAMINATED_FILES,
+        #     nodes=[22, 31],
+        #     window_size=20,
+        #     model_name=ModelName.LOF,
+        #     model_params={"n_neighbors": 20, "contamination": 0.1}
+        # ),
+        # ExperimentConfig(
+        #     config_name="IsolationForest",
+        #     contaminated_files=CONTAMINATED_FILES,
+        #     nodes=[22, 31],
+        #     window_size=0,
+        #     model_name=ModelName.ISOLATION_FOREST,
+        #     model_params={"contamination": 0.1}
+        # ),
+        # ExperimentConfig(
+        #     config_name="OneClassSVM",
+        #     contaminated_files=CONTAMINATED_FILES,
+        #     example_files=CLEAN_FILES,
+        #     nodes=[10, 11, 12, 21, 22, 31, 32],
+        #     window_size=10,
+        #     model_name=ModelName.ONE_CLASS_SVM,
+        #     model_params={"gamma": "scale", "nu": 0.05, "kernel": "rbf"},
+        # ),
         ExperimentConfig(
-            config_name="LOF_20_neighbors",
+            config_name="SVR",
+            contaminated_files=CONTAMINATED_FILES,
             example_files=CLEAN_FILES,
-            contaminated_files=CONTAMINATED_FILES,
-            nodes=[22, 31],
-            window_size=30,
-            model_name=ModelName.LOF,
-            model_params={"n_neighbors": 20, "contamination": 0.1}
+            nodes=[22],
+            window_size=10,
+            model_name=ModelName.SVR,
+            # model_params={"gamma": "scale", "nu": 0.05, "kernel": "rbf"},
         ),
-        ExperimentConfig(
-            config_name="IsolationForest",
-            contaminated_files=CONTAMINATED_FILES,
-            nodes=[22, 31],
-            window_size=0,
-            model_name=ModelName.ISOLATION_FOREST,
-            model_params={"contamination": 0.1}
-        )
     ]
-
     all_results = []
 
     for cfg in configs:
         runner = ExperimentRunner(cfg)
         res = runner.run()
         all_results.append(res)
+    # print(all_results)
 
-    pickle.dump(all_results, open("all_results.pkl", "wb"))
+    # pickle.dump(all_results, open("all_results.pkl", "wb"))
 
-    evaluation = Evaluation()
-    evaluation_results = evaluation.evaluate("all_results.pkl")
+    # evaluation = Evaluation()
+    # evaluation_results = evaluation.evaluate("all_results.pkl")
+    # print(evaluation_results)
 
-    print("Evaluation results:")
+    # print("Evaluation results:")
 
-    evaluation.plot_confusion_matrices("LOF_20_neighbors", evaluation_results)
-    evaluation.plot_results_nodes_within_configurations(Metrics.ACCURACY, evaluation_results)
-    evaluation.plot_mean_configuration(Metrics.ACCURACY, evaluation_results)
+    # evaluation.plot_confusion_matrices("OneClassSVM", evaluation_results)
+    # evaluation.plot_results_nodes_within_configurations(Metrics.ACCURACY, evaluation_results)
+    # evaluation.plot_mean_configuration(Metrics.ACCURACY, evaluation_results)
 
--- experiment/models/SVR.py
+++ experiment/models/SVR.py
@@ -1,10 +1,13 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVR
 
 from typing import List
 
 from sklearn.preprocessing import MinMaxScaler
 from data_transformation import aggregate_data_for_several_nodes, change_data_format, create_features_2, get_data_for_one_node, calculate_labels, create_features
-from model import AnomalyModel
+from models.model import AnomalyModel
 
 # based on https://github.com/microsoft/ML-For-Beginners/blob/main/7-TimeSeries/3-SVR/README.md
 
@@ -28,19 +31,92 @@ class SVRModel(AnomalyModel):
             train = create_features_2(train, self.config.disinfectant.value, self.config.window_size)
             test = create_features_2(test, self.config.disinfectant.value, self.config.window_size)
             
-            scaler = MinMaxScaler()
-            train = scaler.fit_transform(train)
-            test = scaler.transform(test)
-            print(train)
-            print(test)
             
-            # results[node] = {
-            #     "y_true": y_true,
-            #     "y_pred": y_pred
-            # }
+            # get x and y to train on 
+            x_train = np.array([row[:-1] for row in train])
+            y_train = np.array([row[-1] for row in train]).reshape(-1,1)
+
+            # scale the data, two separate scalers are needed to be able to inverse transform the predictions later 'cause different shapes
+            scaler_X = MinMaxScaler()
+            x_train = scaler_X.fit_transform(x_train)
+            scaler_y = MinMaxScaler()
+            y_train = scaler_y.fit_transform(y_train)
+            
+            # params = self.get_best_params(x_train, y_train)
+            # print(params)
+            
+            gamma = self.config.model_params.get("gamma", "scale")
+            kernel = self.config.model_params.get("kernel", "rbf")
+            C = self.config.model_params.get("C", 10)
+            epsilon = self.config.model_params.get("epsilon", 0.01)
+            
+            model = SVR(kernel=kernel,gamma=gamma, C=C, epsilon = epsilon)
+            
+            model.fit(x_train, y_train.ravel())
+            
+            # get x and y to test on
+            x_test = np.array([row[:-1] for row in test])
+            y_test = np.array([row[-1] for row in test]).reshape(-1,1)
+            
+            # scale the test data using the same scalers as for the training data
+            x_test = scaler_X.transform(x_test)
+            y_test = scaler_y.transform(y_test)
+            
+            # reshape y to be in the right format for evaluation
+            y_train_pred = model.predict(x_train).reshape(-1,1)
+            y_test_pred = model.predict(x_test).reshape(-1,1)
+            
+            # inverse transform the predictions to get them back in the original scale
+            y_train_pred = scaler_y.inverse_transform(y_train_pred)
+            y_test_pred = scaler_y.inverse_transform(y_test_pred)
+            
+            y_train = scaler_y.inverse_transform(y_train)
+            y_test = scaler_y.inverse_transform(y_test)
+            
+            # get timestamps for plotting
+            train_timestamps = clean_dfs[i].iloc[:800-self.config.window_size+1]['timestep']
+            test_timestamps = clean_dfs[i].iloc[800+self.config.window_size-1:]['timestep']
+            
+            plt.figure(figsize=(25,6))
+            plt.plot(train_timestamps, y_train, color = 'red', linewidth=2.0, alpha = 0.6)
+            plt.plot(train_timestamps, y_train_pred, color = 'blue', linewidth=0.8)
+            plt.legend(['Actual','Predicted'])
+            plt.xlabel('Timestamp')
+            plt.title("Training data prediction")
+            plt.show()
+            
+            plt.figure(figsize=(10,3))
+            plt.plot(test_timestamps, y_test, color = 'red', linewidth=2.0, alpha = 0.6)
+            plt.plot(test_timestamps, y_test_pred, color = 'blue', linewidth=0.8)
+            plt.legend(['Actual','Predicted'])
+            plt.xlabel('Timestamp')
+            plt.show()
+            
+            
+            results[node] = {
+                "y_true": y_test,
+                "y_pred": y_test_pred
+            }
         
             
         return results
+    
+    def get_best_params(self, x_train, y_train):
+        """Use grid search to find the best hyperparameters for the SVR model."""
+        
+        param_grid = {
+            'C': [0.1, 1, 10, 50, 100, 500, 1000],
+            'epsilon': [0.001, 0.01, 0.1, 0.5, 1],
+            'gamma': ['scale', 0.0001, 0.001, 0.01, 0.1, 1],
+            'kernel': ['rbf']
+        }
+
+        grid = GridSearchCV(SVR(), param_grid, cv=5, scoring='neg_mean_squared_error')
+
+        grid.fit(x_train, y_train.ravel())
+        
+        return grid.best_params_
+    
     
     def load_and_filter(self, file_path: str, nodes: List[int]):
         """Load the dataset from the given file path and filter it based on the specified nodes. Return a list of dataframes corresponding to each node.
