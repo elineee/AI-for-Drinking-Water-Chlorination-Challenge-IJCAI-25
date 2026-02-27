fa23d12869418b005cc029c995156decced634b5
Eline Mota <eline.mota01@gmail.com>
2026-02-27 09:35:30 +0100

    remove useless code

--- experiment/models/LOF.py
+++ experiment/models/LOF.py
@@ -53,30 +53,3 @@ class LOFModel(AnomalyModel):
             results[node] = {"y_true": y_true,"y_pred": y_pred }
             
         return results
-        
-        
-        clean_dfs, contaminated_dfs = self.load_datasets()
-        
-        results = {}
-        
-        for i in range(len(contaminated_dfs)):
-            node = contaminated_dfs[i]['node'].iloc[0] # get node number (should be the same for all rows inside one dataframe)
-            node = str(node)
-
-            X_train = create_features(clean_dfs[i], self.config.disinfectant.value, self.config.window_size)
-            X_test = create_features(contaminated_dfs[i], self.config.disinfectant.value, self.config.window_size)
-
-            # TODO : handle multiple contaminants?
-            y_true = calculate_labels(contaminated_dfs[i], self.config.contaminants[0].value, self.config.window_size)
-            
-            n_neighbors = self.config.model_params.get("n_neighbors", 20)
-            contamination = self.config.model_params.get("contamination", 0.1)
-            
-            lof = LocalOutlierFactor(n_neighbors=n_neighbors, novelty=True, contamination=contamination)
-            lof.fit(X_train)
-
-            y_pred = lof.predict(X_test)
-            
-            results[node] = {"y_true": y_true,"y_pred": y_pred }
-        
-        return results
