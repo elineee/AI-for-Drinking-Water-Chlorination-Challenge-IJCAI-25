fc5ba55abe2e1931c0f4d181b9f711c4005fe440
Eline Mota <eline.mota01@gmail.com>
2026-03-02 15:52:03 +0100

    refactor svr for later use

--- experiment/models/SVR.py
+++ experiment/models/SVR.py
@@ -13,6 +13,98 @@ from models.model import AnomalyModel
 class SVRModel(AnomalyModel):
     """ Class for SVR model"""
     
+    def predict(self, node, clean_dfs, contaminated_dfs): 
+        train = []
+        test = []
+        
+        new_clean_dfs = []
+        # iterate over datasets for each node
+        for i in range(len(clean_dfs)): 
+            train_data = remove_first_x_days(clean_dfs[i], 3)
+            new_clean_dfs.append(train_data)
+            train_data = create_extended_features(train_data, self.config.disinfectant.value, self.config.window_size)
+            train.extend(train_data)
+        train = np.array(train)
+            
+        new_contaminated_dfs = []
+        for i in range(len(contaminated_dfs)): 
+            test_data = remove_first_x_days(contaminated_dfs[i], 3)
+            new_contaminated_dfs.append(test_data)
+            test_data = create_extended_features(test_data, self.config.disinfectant.value, self.config.window_size)
+            test.extend(test_data)
+        test = np.array(test)
+        
+        # get x and y to train on 
+        x_train = np.array([row[:-1] for row in train])
+        y_train = np.array([row[-1] for row in train]).reshape(-1,1)
+
+        # scale the data, two separate scalers are needed to be able to inverse transform the predictions later 'cause different shapes
+        scaler_x = StandardScaler()
+        x_train = scaler_x.fit_transform(x_train)
+        scaler_y = StandardScaler()
+        y_train = scaler_y.fit_transform(y_train)
+        
+        # params = self.get_best_params(x_train, y_train)
+        # print(params)
+        
+        gamma = self.config.model_params.get("gamma", "scale")
+        kernel = self.config.model_params.get("kernel", "rbf")
+        C = self.config.model_params.get("C", 10)
+        epsilon = self.config.model_params.get("epsilon", 0.01)
+        
+        model = SVR(kernel=kernel,gamma=gamma, C=C, epsilon = epsilon)
+        
+        model.fit(x_train, y_train.ravel())
+        
+        # get x and y to test on
+        x_test = np.array([row[:-1] for row in test])
+        y_test = np.array([row[-1] for row in test]).reshape(-1,1)
+        
+        # scale the test data using the same scalers as for the training data
+        x_test = scaler_x.transform(x_test)
+        y_test = scaler_y.transform(y_test)
+        
+        # reshape y to be in the right format for evaluation
+        y_train_pred = model.predict(x_train).reshape(-1,1)
+        y_test_pred = model.predict(x_test).reshape(-1,1)
+        
+        # inverse transform the predictions to get them back in the original scale
+        y_train_pred = scaler_y.inverse_transform(y_train_pred)
+        y_test_pred = scaler_y.inverse_transform(y_test_pred)
+        
+        y_train = scaler_y.inverse_transform(y_train)
+        y_test = scaler_y.inverse_transform(y_test)
+        
+        # Plots
+        train_timestamps = build_timestamps(new_clean_dfs, self.config.window_size)
+        plot_prediction( train_timestamps, y_train, y_train_pred, title=f"Training prediction node {node} ")
+
+        test_timestamps = build_timestamps(new_contaminated_dfs, self.config.window_size)
+        plot_prediction( test_timestamps, y_test, y_test_pred,title=f"Test prediction node {node}")
+                    
+        y_true = calculate_labels(new_contaminated_dfs[i], self.config.contaminants[0].value, self.config.window_size)
+        
+        ok = []
+        ano = []
+        for element in y_true:
+            if element == 1:
+                ok.append(element)
+            else:
+                ano.append(element)
+        print(f"ok: {len(ok)}, ano: {len(ano)}")
+        
+        #calculate the threshold for anomaly detection based on the training data residuals (difference between predicted and true values)
+        residual_train = np.abs(y_train - y_train_pred)
+        threshold = residual_train.mean() + 60 * residual_train.std()
+        
+        print(f"Threshold: {threshold:.4f}")
+        print(len(y_test), len(y_test_pred))
+
+        residual_test = np.abs(y_test - y_test_pred)
+        y_pred = np.where(residual_test > threshold, -1, 1)
+        
+        return y_true, y_pred, y_test, y_test_pred      
+                
     def get_results(self):
         all_clean_dfs, all_contaminated_dfs = self.load_datasets_as_dict()
         
@@ -26,95 +118,8 @@ class SVRModel(AnomalyModel):
             # get contaminated dataset for the same key node
             contaminated_dfs = all_contaminated_dfs[key]
             clean_dfs = value
-
-            train = []
-            test = []
             
-            new_clean_dfs = []
-            # iterate over datasets for each node
-            for i in range(len(clean_dfs)): 
-                train_data = remove_first_x_days(clean_dfs[i], 3)
-                new_clean_dfs.append(train_data)
-                train_data = create_extended_features(train_data, self.config.disinfectant.value, self.config.window_size)
-                train.extend(train_data)
-            train = np.array(train)
-              
-            new_contaminated_dfs = []
-            for i in range(len(contaminated_dfs)): 
-                test_data = remove_first_x_days(contaminated_dfs[i], 3)
-                new_contaminated_dfs.append(test_data)
-                test_data = create_extended_features(test_data, self.config.disinfectant.value, self.config.window_size)
-                test.extend(test_data)
-            test = np.array(test)
-            
-            # get x and y to train on 
-            x_train = np.array([row[:-1] for row in train])
-            y_train = np.array([row[-1] for row in train]).reshape(-1,1)
-
-            # scale the data, two separate scalers are needed to be able to inverse transform the predictions later 'cause different shapes
-            scaler_x = StandardScaler()
-            x_train = scaler_x.fit_transform(x_train)
-            scaler_y = StandardScaler()
-            y_train = scaler_y.fit_transform(y_train)
-            
-            # params = self.get_best_params(x_train, y_train)
-            # print(params)
-            
-            gamma = self.config.model_params.get("gamma", "scale")
-            kernel = self.config.model_params.get("kernel", "rbf")
-            C = self.config.model_params.get("C", 10)
-            epsilon = self.config.model_params.get("epsilon", 0.01)
-            
-            model = SVR(kernel=kernel,gamma=gamma, C=C, epsilon = epsilon)
-            
-            model.fit(x_train, y_train.ravel())
-            
-            # get x and y to test on
-            x_test = np.array([row[:-1] for row in test])
-            y_test = np.array([row[-1] for row in test]).reshape(-1,1)
-            
-            # scale the test data using the same scalers as for the training data
-            x_test = scaler_x.transform(x_test)
-            y_test = scaler_y.transform(y_test)
-            
-            # reshape y to be in the right format for evaluation
-            y_train_pred = model.predict(x_train).reshape(-1,1)
-            y_test_pred = model.predict(x_test).reshape(-1,1)
-            
-            # inverse transform the predictions to get them back in the original scale
-            y_train_pred = scaler_y.inverse_transform(y_train_pred)
-            y_test_pred = scaler_y.inverse_transform(y_test_pred)
-            
-            y_train = scaler_y.inverse_transform(y_train)
-            y_test = scaler_y.inverse_transform(y_test)
-            
-            # Plots
-            train_timestamps = build_timestamps(new_clean_dfs, self.config.window_size)
-            plot_prediction( train_timestamps, y_train, y_train_pred, title=f"Training prediction node {node} ")
-
-            test_timestamps = build_timestamps(new_contaminated_dfs, self.config.window_size)
-            plot_prediction( test_timestamps, y_test, y_test_pred,title=f"Test prediction node {node}")
-                        
-            y_true = calculate_labels(new_contaminated_dfs[i], self.config.contaminants[0].value, self.config.window_size)
-            
-            ok = []
-            ano = []
-            for element in y_true:
-                if element == 1:
-                    ok.append(element)
-                else:
-                    ano.append(element)
-            print(f"ok: {len(ok)}, ano: {len(ano)}")
-            
-            #calculate the threshold for anomaly detection based on the training data residuals (difference between predicted and true values)
-            residual_train = np.abs(y_train - y_train_pred)
-            threshold = residual_train.mean() + 60 * residual_train.std()
-            
-            print(f"Threshold: {threshold:.4f}")
-            print(len(y_test), len(y_test_pred))
-
-            residual_test = np.abs(y_test - y_test_pred)
-            y_pred = np.where(residual_test > threshold, -1, 1)
+            y_true, y_pred, _, _ = self.predict(node, clean_dfs, contaminated_dfs)
   
             results[node] = {"y_true": y_true, "y_pred": y_pred}
                 
