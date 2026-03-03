8abc8c46ef96d1f57cdf0694422ae7ea6ad652c0
AurelieGnt <genot.aurelie.7@gmail.com>
2026-03-03 11:41:41 +0100

    Update model.py
    Deleting functions not used anymore

--- experiment/models/model.py
+++ experiment/models/model.py
@@ -13,32 +13,6 @@ class AnomalyModel(ABC):
     def __init__(self, config: ExperimentConfig):
         self.config = config
 
-    def load_and_filter(self, file_path: str, nodes: List[int]):
-        """
-        Loads the dataset from the given file path and filters it based on the specified nodes. It returns a list of dataframes corresponding to each node.
-        
-        Parameters:
-        - file_path: the path to the data file (csv) to load
-        - nodes: a list of node numbers to filter the data by
-        
-        Returns:
-        - a list of pandas DataFrames, each containing the data for one of the specified nodes
-        """
-        # TODO : add parameters contaminants when changed in function 
-        df_all = change_data_format(file_path, self.config.contaminants, to_csv=False)  # returns rows with columns: timestep, node, chlorine_concentration, arsenic_concentration
-        
-        dfs = []
-        
-        if self.config.aggregate_method is None:
-            for node in nodes:
-                df_node = get_data_for_one_node(df_all, node, to_csv=False)
-                dfs.append(df_node)
-        
-        else:
-            df = aggregate_data_for_several_nodes(df_all, nodes, method=self.config.aggregate_method, to_csv=False)
-            dfs.append(df)
-        
-        return dfs
 
     def load_and_filter_as_dict(self, file_path: str, nodes: List[int]):
         """
@@ -69,30 +43,9 @@ class AnomalyModel(ABC):
         return dfs
 
 
-    def load_datasets(self):
-        """
-        Loads the datasets for the experiment based on the examples files and contamined files specified in the configuration.
-        Return lists of dataframes for each contaminated and example (clean) file. 
-
-        Returns:
-        example_dfs: list of dataframes for each example file (clean data)
-        contamined_dfs: list of dataframes for each contaminated file
-        """ 
-        example_dfs = []
-        if self.config.example_files is not None:
-            for fp in self.config.example_files:
-                example_dfs.extend(self.load_and_filter(fp, self.config.nodes))
-
-        contaminated_dfs = []
-        for fp in self.config.contaminated_files:
-            contaminated_dfs.extend(self.load_and_filter(fp, self.config.nodes))
-
-        return example_dfs, contaminated_dfs
-
-
     def _group_dfs_by_node(self, files: List[str]):
         """
-        Group dataframes by node for the given list of files. 
+        Groups dataframes by node for the given list of files. 
 
         Parameters:
         - files: list of files to load and group by node
@@ -161,9 +114,18 @@ class AnomalyModel(ABC):
         return datasets, np.array(windows)
     
     def _post_predictions(self, y_pred):
+        """
+        Does post predictions on the predicted labels 
+
+        Parameters: 
+        - y_pred: predicted labels
+        """
         return y_pred
     
     def _calculate_labels(self, df, contaminant, window_size):
+        """
+        Gets the method to calculate labels. 
+        """
         return calculate_labels(df, contaminant, window_size)
     
     @abstractmethod
