ccbce1d8c43cf04a5fbb609f0380c9a5b17e75ca
Eline Mota <eline.mota01@gmail.com>
2026-03-18 16:07:35 +0100

    add data augmentation for cnn univariate

--- experiment/models/CNN_univariate.py
+++ experiment/models/CNN_univariate.py
@@ -1,10 +1,56 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 import torch
-from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import Dataset, TensorDataset, DataLoader
 from data_transformation import remove_first_x_days, calculate_labels_alarm
 from utils import detect_change_point
 from models.CNN import CNNModel
+
+class TimeSeriesAugmentation:
+    def __init__(self, transforms):
+        self.transforms = transforms
+        
+    def __call__(self, data):
+        for transform in self.transforms:
+            data = transform(data)
+        return data
+
+class AugmentedTensorDataset(Dataset):
+    def __init__(self, data, labels, augment=None):
+        self.data = data
+        self.labels = labels
+        self.augment = augment
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        x = self.data[idx]
+        y = self.labels[idx]
+
+        if self.augment is not None:
+            x = self.augment(x)
+            
+        return x, y
+
+def gaussian_noise(data, mean=0.0, prob=0.5):
+    if np.random.rand() > prob:
+        return data
+    std_values = [0.01, 0.03, 0.05] 
+    std = np.random.choice(std_values)  
+    noise = np.random.normal(mean, std, data.shape)
+    data = data + noise 
+    data = data.float()
+    return data 
+
+def blank_value(data, percentage=0.025, prob=0.5):
+    if np.random.rand() > prob:
+        return data
+    num_blank = int(percentage * data.numel())
+    indices = np.random.choice(data.numel(), num_blank, replace=False)
+    data[indices] = 0.0
+    data = data.float()
+    return data
 
 class CNNUnivariateModel(CNNModel):
     """ Class for CNN model. It takes into account the raw signal (univariate)"""
@@ -51,17 +97,18 @@ class CNNUnivariateModel(CNNModel):
             X_train, X_val, y_train, y_val = train_test_split(data_train, y_train, test_size=0.15, random_state=42)
 
             # create DataLoaders
-            train_dataset = TensorDataset(X_train, y_train)
+            train_dataset = AugmentedTensorDataset(X_train, y_train, augment=TimeSeriesAugmentation([gaussian_noise, blank_value]))
+            # train_dataset = TensorDataset(X_train, y_train)
             val_dataset = TensorDataset(X_val, y_val)
             test_dataset = TensorDataset(data_test, y_test)
-            train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True) # one batch = (32, 48)
-            val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+            train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True) # one batch = (32, 48)
+            val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=False)
             test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False)
             
 
             weights = self._compute_weight(y_train)
-            y_pred = self.run_model(train_dataloader, val_dataloader, test_dataloader, weights, epochs=20)
-            y_pred = detect_change_point(y_pred, count_required=5)
+            y_pred = self.run_model(train_dataloader, val_dataloader, test_dataloader, weights, epochs=15)
+            y_pred = detect_change_point(y_pred, count_required=15)
             results[node] = {"y_pred": y_pred, "y_true": y_true}
         
         return results
