b37822ac6fd5f12366056ca4395ee5acc3693c1e
AurelieGnt <aurelie.genot@student.unamur.be>
2026-04-25 08:43:20 +0200

    VAE CNN Delay 20

--- experiment/models/VAE_CNN.py
+++ experiment/models/VAE_CNN.py
@@ -292,7 +292,7 @@ class VAECNNModel(CNNModel):
                 
             weights = self._compute_weight(y_train)
             y_pred = self.run_model(train_dataloader, val_dataloader, test_dataloader, weights, epochs=200, patience=20)
-            y_pred = detect_change_point(y_pred, count_required=15)
+            y_pred = detect_change_point(y_pred, count_required=20)
             results[node] = {"y_pred": y_pred, "y_true": y_true}
         
         return results
